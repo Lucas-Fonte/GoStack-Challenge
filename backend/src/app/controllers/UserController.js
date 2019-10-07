10a0d1c30e447360d15367ac7020c68834b1c087
Lucas Souza da Fonte <lucasfonte@Lucass-MacBook-Pro.local>
2019-10-07 12:19:59 -0300

    first commit

---
+++ backend/src/app/controllers/UserController.js
@@ -0,0 +1,30 @@
+import User from '../models/User';
+
+class UserController {
+    async store(req, res) {
+        const userExists = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+
+        if (userExists) {
+            return res.json({ error: 'User already exists' });
+        }
+        const { id, name, email } = await User.create(req.body);
+
+        return res.json({
+            id,
+            name,
+            email
+        });
+    }
+
+    async update(req, res) {
+        console.log(req.userId);
+
+        res.json({ ok: true });
+    }
+}
+
+export default new UserController();
