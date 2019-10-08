d497d8d1d144be721bc61fa7a067d8d74cf16bd0
Lucas Souza da Fonte <lucasfonte@Lucass-MacBook-Pro.local>
2019-10-08 12:43:32 -0300

    axios connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import api from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    result: '',
+    name: ''
+  }
+
+  handleResponse = async e => {
+    const response = await api.post('/sessions', {
+      email: 'lucas@gmail.com',
+      password: '1234'
+
+    });
+
+    console.log(response);
+      this.setState({
+        result: response,
+        name: response.data.user.name
+      })
+    };
+
+  render(){
+    const { result, name } = this.state;
+
+    return (
+        <>
+          <button onClick={this.handleResponse}></button>
+          <h1>{name}</h1>
+        </>
+      );
+  }
 }
 
-export default App;
+
+
